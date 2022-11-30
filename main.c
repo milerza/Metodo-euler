cf410eb5fb5349637f4bf0e5ed30a32970b5db6f
Camila Silva Alves <alvescamila0116@gmail.com>
2022-11-30 15:00:32 -0300

    corrigindo a contante c

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-//#include <conio.h>
 #include <math.h>
 
 #define f(x, y) x + y
 
 double C(double gama_t){
-    return 2*gama_t;
+    return 2; // c is const
+    //return 2*gama_t; //c depends of  x
 } 
 
-double U0(double psi){
-    return exp(-pow(psi, 2));
+double U0(double gama){
+    return exp(-pow(gama, 2));
 }
 
 int main(){
     double t_0, gama_0, t_n, c, gama_n, dt, u, psi;
     int i, n;
-    //clrscr();
+
     printf("Condições iniciais\n");
     printf("t_0 = ");
     scanf("%lf", &t_0);
@@ -26,8 +26,12 @@ int main(){
     printf("Quantidade de iteracoes: ");
     scanf("%d", &n);
 
-    /* Calculating step size (h) */
+    /* Calculating step size */
     dt = (t_n-t_0)/n;
+
+    // U(x,t) is constant in t 
+
+    u = U0(gama_0);
 
     /* Euler's Method */
     printf("\nt\tgama_n\tu\tdt = %.4f \n", dt);
@@ -35,9 +39,7 @@ int main(){
     for(i=0; i < n; i++){
         c = C(gama_0);
         gama_n = gama_0 + c * dt;
-        psi = gama_n * exp(-2.0*t_0);
-        u = U0(psi);
-        printf("%.4f\t%.4f\t%.4f\n",t_0,gama_n, u);
+        printf("%.2f\t%.4f\t%.6f\t%.6f\n",t_0,gama_0, u, c);
         gama_0 = gama_n;
         t_0 = t_0 + dt;
     }
@@ -45,6 +47,5 @@ int main(){
     /* Displaying result */
     printf("\nValue of gama at t = %0.2f is %0.3f",t_n, gama_n);
 
-    //getch();
     return 0;
 }
